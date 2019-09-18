d288d36c6a5a27562981586ab2e167d545db9aef
Tomas Nozicka <tnozicka@gmail.com>
2019-09-18 17:09:53 +0200

    sync

--- alpha-build-machinery/make/targets/openshift/crd-schema-gen.mk
+++ alpha-build-machinery/make/targets/openshift/crd-schema-gen.mk
@@ -1,36 +1,53 @@
-CRD_SCHEMA_GEN_VERSION ?=v0.2.1
-CRD_SCHEMA_GEN_APIS ?=$(error CRD_SCHEMA_GEN_APIS is required) 
-CRD_SCHEMA_GEN_TEMP ?=_output/tools/src/sigs.k8s.io/controller-tools
+self_dir :=$(dir $(lastword $(MAKEFILE_LIST)))
+
+CRD_SCHEMA_GEN_APIS ?=$(error CRD_SCHEMA_GEN_APIS is required)
+CRD_SCHEMA_GEN_MANIFESTS ?=./manifests
 CRD_SCHEMA_GEN_OUTPUT ?=./manifests
 
-crd-schema-gen:
-	if [ ! -d $(CRD_SCHEMA_GEN_TEMP) ]; then \
-		mkdir -p $(CRD_SCHEMA_GEN_TEMP); \
-		git clone -b $(CRD_SCHEMA_GEN_VERSION) --single-branch --depth 1 https://github.com/kubernetes-sigs/controller-tools.git $(CRD_SCHEMA_GEN_TEMP); \
-		mkdir -p $(CRD_SCHEMA_GEN_TEMP)/bin; \
-		export GOPATH=$(shell pwd)/_output/tools/ && export GOBIN=$(shell pwd)/_output/tools/bin/ && cd $(CRD_SCHEMA_GEN_TEMP) && export GO111MODULE=on && go mod vendor && go install -mod=vendor ./cmd/controller-gen/...; \
-	fi
-.PHONY: crd-schema-gen
+crd_patches :=$(subst $(CRD_SCHEMA_GEN_MANIFESTS),$(CRD_SCHEMA_GEN_OUTPUT),$(wildcard $(CRD_SCHEMA_GEN_MANIFESTS)/*.crd.yaml-merge-patch))
 
-update-codegen-crds: crd-schema-gen
-	./_output/tools/bin/controller-gen \
-		schemapatch:manifests=./manifests \
-		output:dir=$(CRD_SCHEMA_GEN_OUTPUT) \
-		paths="$(subst $() $(),;,$(CRD_SCHEMA_GEN_APIS))"
+# $1 - crd file
+# $2 - patch file
+define patch-crd
+	$(YQ) m -i '$(1)' '$(2)'
+
+endef
+
+empty :=
+update-codegen-crds: ensure-controller-gen ensure-yq
+	'$(CONTROLLER_GEN)' \
+		schemapatch:manifests="$(CRD_SCHEMA_GEN_MANIFESTS)" \
+		paths="$(subst $(empty) ,;,$(CRD_SCHEMA_GEN_APIS))" \
+		output:dir="$(CRD_SCHEMA_GEN_OUTPUT)"
+	cp -n $(crd_patches) $(CRD_SCHEMA_GEN_OUTPUT)/
+	$(foreach p,$(crd_patches),$(call patch-crd,$(basename $(p)).yaml,$(p)))
 .PHONY: update-codegen-crds
 
-# crd-schema-update-with-patch should be used for repos with yaml patches. It calls update-codegen-crds then applies any patches after first copying any patch files to the output directory (to ensure that verify works since that target runs in a temp dir). It depends on the `yq` tool to merge yaml
-crd-schema-update-with-patch: update-codegen-crds
-	if [ ! -f ./_output/tools/bin/yq ]; then \
-		curl -f -L -o ./_output/tools/bin/yq https://github.com/mikefarah/yq/releases/download/2.4.0/yq_$(shell uname -s | tr A-Z a-z)_amd64 && chmod +x ./_output/tools/bin/yq; \
-	fi
-	if [ $(CRD_SCHEMA_GEN_OUTPUT) != "./manifests" ]; then \
-		cp -n ./manifests/*.crd.yaml-merge-patch $(CRD_SCHEMA_GEN_OUTPUT)/.; \
-	fi
-	set -euo pipefail && for p in $(CRD_SCHEMA_GEN_OUTPUT)/*.crd.yaml-merge-patch; do ./_output/tools/bin/yq m -i "$${p%%.crd.yaml-merge-patch}.crd.yaml" "$$p"; done
-.PHONY: crd-schema-patch
+update-generated: update-codegen-crds
+.PHONY: update-generated
+
+update: update-generated
+.PHONY: update
+
 
 verify-codegen-crds: CRD_SCHEMA_GEN_OUTPUT :=$(shell mktemp -d)
-verify-codegen-crds: crd-schema-update-with-patch
-	diff -Naup ./manifests $(CRD_SCHEMA_GEN_OUTPUT)
+verify-codegen-crds: update-codegen-crds
+	diff -Naup './manifests' '$(CRD_SCHEMA_GEN_OUTPUT)'
 .PHONY: verify-codegen-crds
+
+verify-generated: verify-codegen-crds
+.PHONY: verify-generated
+
+verify: verify-generated
+.PHONY: verify
+
+
+# We need to be careful to expand all the paths before any include is done
+# or self_dir could be modified for the next include by the included file.
+# Also doing this at the end of the file allows us to user self_dir before it could be modified.
+include $(addprefix $(self_dir), \
+	../../lib/golang.mk \
+	../../lib/tmp.mk \
+	../../targets/openshift/controller-gen.mk \
+	../../targets/openshift/yq.mk \
+)
